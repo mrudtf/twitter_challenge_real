2df675d69c4183e767985fdca3e3a325478cc1b7
Lyndon R. Labayan <lyndon.labayan@gmail.com>
2013-11-06 17:05:04 -0600

    First commit, as usual!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterChallengeReal::Application.config.secret_token = '7778ec2aeba3bb2dd023dbf8443ecda8023366c147d5a753456646a1e9f7b412f279be1205d78287844767b8986d67022496a6a5cf19a3df20cf158dae531c50'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TwitterChallengeReal::Application.config.session_store :cookie_store, key: '_twitter_challenge_real_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TwitterChallengeReal::Application.config.session_store :active_record_store
